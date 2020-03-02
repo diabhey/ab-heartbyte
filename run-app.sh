100d8f0113f4ca033e230d20277ed0548fa148fd
aselvan <aselvan@oceaneering.com>
2020-03-02 17:32:46 +0100

    Added script to run app locally

---
+++ run-app.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+
+function run(){
+    pip install -r requirements.txt && 
+    export FLASK_APP=application.py && 
+    flask run
+}
+
+echo "Note: Run the app in your own virtual environment(venv)!
+      Example:  python3 -m venv heartbyte-venv
+                source heartbyte/bin/activate"
+if { python3 --version | grep -q "^Python[[:space:]]*[3]\.[[:digit:]]*[\.]*[[:digit:]]*$"; } then
+    run;
+else
+    echo "Install Python 3!";
+fi
+
+
+
+
+
