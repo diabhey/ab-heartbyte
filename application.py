c711bef9311c3b3562af51a81daa0b87e82f99e3
bigillu <abhimanyuselvan@protonmail.com>
2020-03-20 17:22:07 +0100

    flask-opencv test app

--- application.py
+++ application.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template
-from config import Config
+import time
+import cv2
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
-@app.route("/")
-def hb():
-    return render_template('home.html', title='HeartByte')
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
 
+def gen():
+    """Video streaming generator function."""
+    cap = cv2.VideoCapture(-1)
+
+    # Read until video is completed
+    while(cap.isOpened()):
+      # Capture frame-by-frame
+        ret, img = cap.read()
+        if ret == True:
+            img = cv2.resize(img, (0,0), fx=0.5, fy=0.5) 
+            frame = cv2.imencode('.jpg', img)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            time.sleep(0.1)
+        else: 
+            break
+    
 if __name__ == '__main__':
-    app.run(debug=True)    
+    app.run(host='0.0.0.0', debug=True)
+
+
