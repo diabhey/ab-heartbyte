4d9664441fddcc89f9106ff9f6462366f221375b
bigillu <abhimanyuselvan@protonmail.com>
2020-04-05 18:28:51 +0200

    Removing face_recognition dependency

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 import cv2
-import face_recognition
 from flask import Flask, render_template, Response
 
 app = Flask(__name__)
@@ -14,8 +13,6 @@ def gen():
         ret, frame = video.read()
         if ret == True:
             frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5) 
-            # face_locations = face_recognition.face_locations(frame, model="cnn")
-            # encodings = face_recognition.face_encodings(frame, face_locations)
             frame = cv2.imencode('.jpg', frame)[1].tobytes()
             yield (b'--frame\r\n'b'Content-Type: frame/jpeg\r\n\r\n' + frame + b'\r\n')                
 
