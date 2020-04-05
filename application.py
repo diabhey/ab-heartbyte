9198bb0165c55d540bf3e16a3238e6ee30b88675
bigillu <abhimanyuselvan@protonmail.com>
2020-04-05 18:57:12 +0200

    Removing opencv dependency

--- application.py
+++ application.py
@@ -1,4 +1,3 @@
-import cv2
 from flask import Flask, render_template, Response
 
 app = Flask(__name__)
@@ -7,25 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-def gen():
-    video = cv2.VideoCapture(0)
-    while(video.isOpened()):
-        ret, frame = video.read()
-        if ret == True:
-            frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5) 
-            frame = cv2.imencode('.jpg', frame)[1].tobytes()
-            yield (b'--frame\r\n'b'Content-Type: frame/jpeg\r\n\r\n' + frame + b'\r\n')                
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                   break
-        else:
-            break           
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')            
-    
 if __name__ == '__main__':
     app.run(debug=True)
 
