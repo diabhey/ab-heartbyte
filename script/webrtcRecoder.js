d3475d8b1b3985a193b31422237fba5ed8da3b07
bigillu <abhimanyuselvan@protonmail.com>
2020-04-10 21:59:28 +0200

    webrtc cam recorder to save the stream

---
+++ script/webrtcRecoder.js
@@ -0,0 +1,21 @@
+var mediaConstraints = {
+    audio: true,
+    video: true
+};
+
+navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
+
+function onMediaSuccess(stream) {
+    var mediaRecorder = new MediaStreamRecorder(stream);
+    mediaRecorder.mimeType = 'video/webm';
+    mediaRecorder.ondataavailable = function (blob) {
+        // POST/PUT "Blob" using FormData/XHR2
+        var blobURL = URL.createObjectURL(blob);
+        document.write('<a href="' + blobURL + '">' + blobURL + '</a>');
+    };
+    mediaRecorder.start(3000);
+}
+
+function onMediaError(e) {
+    console.error('media error', e);
+}
