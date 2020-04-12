4db29edabea2af639820d6ac055fe8426f173075
bigillu <abhimanyuselvan@protonmail.com>
2020-04-12 12:28:32 +0200

    Setting up MongoDb cluster on Azure

--- application.py
+++ application.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, Response
+import pymongo
+from pymongo import MongoClient
+
+# Setting up Mongodb 
+cluster = MongoClient("---azure.mongodb---") 
+db = cluster["heartbyte"]
+collection = db["record"]
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug = True)  
-
-
-
-
-
-
