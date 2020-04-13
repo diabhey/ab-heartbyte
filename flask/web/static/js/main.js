53e2f8a5b72b67c96d13986c1e91b85b7f9df5d9
Andrei Gherghescu <gandrein@protonmail.com>
2020-04-13 15:16:32 +0200

    Temporary fix that makes recoding work on Firefox
    See issue here
    https://github.com/onfido/onfido-sdk-ui/issues/891
    
    See solution to detect browser version in JavaScript
    https://stackoverflow.com/questions/5916900/how-can-you-detect-the-version-of-a-browser

--- flask/web/static/js/main.js
+++ flask/web/static/js/main.js
@@ -76,6 +76,11 @@ function handleDataAvailable(event) {
 function startRecording() {
     recordedBlobs = [];
     let options = { mimeType: 'video/webm;codecs=vp9' };
+    if (browserType() === 'Firefox') {
+        // codecs=vp9 is not supported on Firefox
+        options = { mimeType: 'video/webm;codecs=vp8,opus' };
+    }
+
     if (!MediaRecorder.isTypeSupported(options.mimeType)) {
         console.error(`${options.mimeType} is not Supported`);
         errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
@@ -126,6 +131,30 @@ function handleSuccess(stream) {
     gumVideo.srcObject = stream;
 }
 
+function browserType() {
+    navigator.sayswho = (function() {
+        var ua = navigator.userAgent,
+            tem,
+            M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if (/trident/i.test(M[1])) {
+            tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+            return 'IE ' + (tem[1] || '');
+        }
+        if (M[1] === 'Chrome') {
+            tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+            if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+        }
+        // TODO: enable check on browser version but do it differently
+        // M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+        // if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+        // return M.join(' ');
+
+        return M[1];
+    })();
+
+    console.log(navigator.sayswho);
+    return navigator.sayswho;
+}
 async function init(constraints) {
     try {
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
