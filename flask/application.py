99bf8d2d66020d8f363544519adf4bdb19a7108f
bigillu <abhimanyuselvan@protonmail.com>
2020-04-14 14:19:49 +0200

    Removing all things MongoDB

--- flask/application.py
+++ flask/application.py
@@ -1,11 +1,4 @@
 from flask import Flask, render_template, Response
-import pymongo
-from pymongo import MongoClient
-
-# Setting up Mongodb
-cluster = MongoClient("---azure.mongodb---")
-db = cluster["heartbyte"]
-collection = db["record"]
 
 app = Flask(__name__,
             static_url_path='',
